91730f99be3d7e180e7f5fff9d96472a09ac8613
wh_feng <weihan.feng.contact@gmial.com>
2023-05-12 19:40:16 +0800

    Add simple test case

---
+++ src/tests/modeling/test_unet.py
@@ -0,0 +1,10 @@
+import pytest
+import torch
+import pipeline as pl
+
+def test_UNET():
+    x = torch.randn((5, 3, 256, 256))
+    model = pl.modeling.unet.UNet(in_channels=3, out_channels=1)
+    preds = model(x)
+    
+    assert preds.shape == (5, 1, 256, 256), "Output shape is incorrect"
