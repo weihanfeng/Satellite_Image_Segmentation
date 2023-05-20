a94d2fa9685b0b4826551999df05fdbf87646cd2
wh_feng <weihan.feng.contact@gmial.com>
2023-05-21 01:57:00 +0800

    Add description for progress bar

--- src/pipeline/ingestion/data_ingestion.py
+++ src/pipeline/ingestion/data_ingestion.py
@@ -20,7 +20,7 @@ class DataIngestion:
         response = requests.get(self.url, stream=True)
         total_size_in_bytes = int(response.headers.get('content-length', 0))
         block_size = 1024
-        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc="Downloading data")
         with open(self.file_path, 'wb') as file:
             for data in response.iter_content(block_size):
                 progress_bar.update(len(data))
