610f1c1248c7d1c447431953a7a85e997e100412
wh_feng <weihan.feng.contact@gmial.com>
2023-05-20 23:51:02 +0800

    Update import

--- src/tests/modeling/test_unet.py
+++ src/tests/modeling/test_unet.py
@@ -4,7 +4,7 @@ import pipeline as pl
 
 def test_UNET():
     x = torch.randn((5, 3, 256, 256))
-    model = pl.modeling.unet.UNet(in_channels=3, out_channels=1)
+    model = pl.modeling.models.UNet(in_channels=3, out_channels=1)
     preds = model(x)
     
     assert preds.shape == (5, 1, 256, 256), "Output shape is incorrect"
