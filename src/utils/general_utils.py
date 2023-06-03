91ac4364a2f95850eb0371e3ef289eff56be8dcb
wh_feng <weihan.feng.contact@gmial.com>
2023-06-03 19:33:59 +0800

    Add predict functions

--- src/utils/general_utils.py
+++ src/utils/general_utils.py
@@ -5,6 +5,11 @@ import yaml
 import torch
 import cv2
 import matplotlib.pyplot as plt
+from patchify import patchify, unpatchify
+import numpy as np
+import torch.nn.functional as F
+import torchvision.transforms as T
+
 
 logger = logging.Logger(__name__)
 
@@ -50,6 +55,65 @@ def load_model(model_dir, cpu=False):
     else:
         model = torch.load(model_dir)
     return model
+
+def _pad_img(img, window_size):
+    """
+    pad image on the right and bottom to make it divisible by window size
+    """
+    h, w, c = img.shape
+    pad_h = 0
+    pad_w = 0
+    if h % window_size != 0:
+        pad_h = window_size - (h % window_size)
+    if w % window_size != 0:
+        pad_w = window_size - (w % window_size)
+    img = np.pad(
+        img,
+        ((0, pad_h), (0, pad_w), (0, 0)),
+        mode="reflect",
+    )
+    return img, pad_h, pad_w
+
+def _unpad_mask(img, pad_h, pad_w):
+    """
+    unpad mask
+    """
+    h, w = img.shape
+    img = img[: h - pad_h, : w - pad_w]
+    return img
+
+def predict_image(image, model, patch_size):
+    """
+    Predict image mask using sliding window.
+    Image is a numpy array of shape (h, w, c).
+    """
+    # pad image
+    image, pad_h, pad_w = _pad_img(image, patch_size)
+    # patchify image
+    patches = patchify(image, (patch_size, patch_size, 3), step=patch_size)
+    patch_shape = patches.shape
+    # predict patches
+    patches = patches.reshape(-1, patch_size, patch_size, 3)
+    patches = torch.stack(
+        [
+            T.ToTensor()(np.squeeze(np.expand_dims(image, axis=0), axis=0))
+            for image in patches
+        ]
+    )
+    predictions = model(patches)
+    predictions = torch.permute(predictions, (0, 2, 3, 1))
+    predictions = F.softmax(predictions, dim=3)
+    predictions = torch.argmax(predictions, dim=3)
+    predictions = predictions.numpy()
+    # reshape into patch_shape
+    predictions = predictions.reshape(patch_shape[0], patch_shape[1], patch_size, patch_size)
+    # unpatchify
+    predictions = unpatchify(predictions, image.shape[:-1])
+    # unpad
+    predictions = _unpad_mask(predictions, pad_h, pad_w)
+    # prediction is a numpy array of shape (h, w)
+
+    return predictions
 
 def view_image_and_mask(image_path, mask_path):
     """
