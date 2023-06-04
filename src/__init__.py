184b95dc730c933ae7f073c40c9aef0403494212
wh_feng <weihan.feng.contact@gmial.com>
2023-06-04 19:12:03 +0800

    Modify init py

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from . import pipeline
+# from . import get_and_process_data
+# from . import train_model
+from . import utils
--- src/pipeline/modeling/__init__.py
+++ src/pipeline/modeling/__init__.py
@@ -1,2 +1,3 @@
 from . import dataset
 from . import models
+from . import trainer
